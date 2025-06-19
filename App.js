2b233f20444373f5d0b99f6b9f2a3e165735449d
sushmita-sush20 <sushmita2899@gmail.com>
2025-06-20 00:00:17 +0530

    Remove node_modules from repo and update .gitignore

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
+ import React from "react";
+ import ReactDOM from "react-dom/client";
+
  const heading = React.createElement("div", 
     {
         id : "parent"
     }, 
-    [React.createElement("div" ,{id: "child"},
+    [React.createElement("div" ,{id: "child1", key: "child1"},
         [
-            React.createElement("h1", {}, "Iam an h1 tag"),
-            React.createElement("h2", {}, "Iam an h2 tag")
+            React.createElement("h1", {key: "c1h1"}, "Iam an h1 tag"),
+            React.createElement("h2", {key: "c1h2"}, "Iam an h2 tag")
         ]
     ),
-    React.createElement("div" ,{id: "child2"},
+    React.createElement("div" ,{id: "child2", key: "child2"},
         [
-            React.createElement("h1", {}, "Iam an h1 tag"),
-            React.createElement("h2", {}, "Iam an h2 tag")
+            React.createElement("h1", {key: "c2h1"}, "Iam an h1 tag"),
+            React.createElement("h2", {key: "c2h2"}, "Iam an h2 tag")
         ]
     )
 ]);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Basic React
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in c++
+- Caching = Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and production bundles
