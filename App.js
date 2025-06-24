44f9d46e7b54d31be13c7ab1985d46d98d39ac91
sushmita-sush20 <sushmita2899@gmail.com>
2025-06-24 12:47:33 +0530

    Added assignments

--- App.js
+++ App.js
@@ -18,5 +18,24 @@
         ]
     )
 ]);
+
+//React Element - JS Object
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
+
+//JSx
+const jsxHeading = <h1 id = "heading">Namaste React using JSX</h1>;
+console.log(jsxHeading);
+root.render(jsxHeading);
+
+//React Component
+const HeadingComponent = () => (
+<h1 className = "heading">Namaste React Functional Component</h1>
+);
+root.render(<HeadingComponent/>);
+
+//Component Composition
+const Title = () => (
+    <><HeadingComponent /><h2>This is a component Composition</h2></>
+);
+root.render(<Title/>);
